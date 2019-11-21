3fef4eccc8f8e55b961cd8f7f1af6e36685317da
Simao Meneses <simaorsmeneses@gmail.com>
2019-11-21 19:42:56 +0000

    Lab done

--- starter-code/routes/authentication.js
+++ starter-code/routes/authentication.js
@@ -12,25 +12,112 @@ router.get('/sign-up', (req, res, next) => {
   res.render('sign-up');
 });
 
+let token = ''; 
+
+const generateId = length => {
+  
+  const characters =
+    '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+ 
+  for (let i = 0; i < length; i++) {
+    token += characters[Math.floor(Math.random() * characters.length)];
+  }
+  return token;
+};
+
+const nodemailer = require('nodemailer');
+
+const EMAIL = 'ih174test@gmail.com';
+const PASSWORD = 'IH174@lis';
+
+const transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: EMAIL,
+    pass: PASSWORD
+  }
+});
+
 router.post('/sign-up', (req, res, next) => {
   const { name, email, password } = req.body;
+  
+  generateId(12)
+  
   bcryptjs
     .hash(password, 10)
     .then(hash => {
       return User.create({
         name,
         email,
-        passwordHash: hash
+        passwordHash: hash,
+        confirmationCode: token
       });
     })
     .then(user => {
       req.session.user = user._id;
+      return transporter.sendMail({
+        from: `IH Test <${EMAIL}>`,
+        to: user.email,
+        subject: 'testatoa',
+        // text: 'This should be the body of the text email'
+        html: `
+          
+          <h1 style="color: green">This should be the body of the text email</h1>
+          <p><strong>Hello</strong> <em>World!</em></p>
+         <a href = "http://localhost:3000/auth/confirm/${user.confirmationCode}">click to confirm </a>
+
+        `
+      });
+    })
+    .then(response => {
+      console.log(response);
       res.redirect('/');
+    })
+    .catch(error => {
+      console.log(error);
     })
     .catch(error => {
       next(error);
     });
+
+  
+
+
 });
+
+
+router.get('/auth/confirm/:reference',(req, res, next) => {
+  const linkvalue = req.params.reference;
+
+  User.findOneAndUpdate({confirmationCode: linkvalue},{status: "Active"})
+
+  .then(response => {
+    console.log(response);
+    res.redirect('/profile');
+  })
+
+  .catch(error => {
+    next(error);
+  });
+  
+  
+}); 
+
+
+
+
+router.get('/profile', (req, res, next) => {
+  User.findById(req.session.user).then(user=>{
+    res.render('profile', {user} ) 
+  }).catch(error => {
+    next(error);
+  });
+
+
+ 
+});
+
+
 
 router.get('/sign-in', (req, res, next) => {
   res.render('sign-in');
