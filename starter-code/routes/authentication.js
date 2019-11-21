decd612d2113bf43fae82e2d3bc20b68eae8e54b
FilipeMts <jfgmatos@outlook.com>
2019-11-21 22:45:31 +0000

    done

--- starter-code/routes/authentication.js
+++ starter-code/routes/authentication.js
@@ -1,49 +1,104 @@
-const { Router } = require('express');
+'use strict'
+
+const { Router } = require("express");
 const router = new Router();
+const nodemailer = require("nodemailer");
+const User = require("./../models/user");
+const bcryptjs = require("bcryptjs");
 
-const User = require('./../models/user');
-const bcryptjs = require('bcryptjs');
+const generateId = length => {
+  const characters =
+    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let token = "";
+  for (let i = 0; i < length; i++) {
+    token += characters[Math.floor(Math.random() * characters.length)];
+  }
+  return token;
+};
 
-router.get('/', (req, res, next) => {
-  res.render('index');
+let transporter = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD
+  }
 });
 
-router.get('/sign-up', (req, res, next) => {
-  res.render('sign-up');
+const sendMail = user => {
+  transporter.sendMail({
+    from: `IH Test <MAIL>`,
+    to: `${user.email}`,
+    subject: "Email verification",
+    html: `
+    <img src="http://cliparts.co/cliparts/6iy/5y5/6iy5y58KT.png" width="200px"/>
+    <p>Please verify your email address by clicking the following link: <a href="http://localhost:3000/confirm/${user.confirmationCode}">confirm</a></p>` 
+  });
+}
+
+router.get("/", (req, res, next) => {
+  res.render("index");
 });
 
-router.post('/sign-up', (req, res, next) => {
+router.get("/success", (req, res, next) => {
+  res.render("success");
+  console.log(req.user);
+});
+
+router.get("/confirm/:token", (req, res, next) => {
+  const token = req.params.token;
+  User.findOneAndUpdate({confirmationCode: token}, 
+    { 
+      status: "Active" 
+    })
+    .then(user => {
+      req.session.user = user._id;
+      res.redirect("/success");
+    })
+    .catch(err => {
+      next(err)
+    });
+});
+
+router.get("/sign-up", (req, res, next) => {
+  res.render("sign-up");
+});
+
+router.post("/sign-up", (req, res, next) => {
   const { name, email, password } = req.body;
+  const confirmToken = generateId(30);
   bcryptjs
-    .hash(password, 10)
+    .hash(password, 15)
     .then(hash => {
       return User.create({
         name,
         email,
-        passwordHash: hash
+        passwordHash: hash,
+        confirmationCode: confirmToken
       });
     })
     .then(user => {
+      sendMail(user);
       req.session.user = user._id;
-      res.redirect('/');
+      res.redirect("/");
     })
     .catch(error => {
       next(error);
     });
 });
 
-router.get('/sign-in', (req, res, next) => {
-  res.render('sign-in');
+router.get("/sign-in", (req, res, next) => {
+  res.render("sign-in");
 });
 
-router.post('/sign-in', (req, res, next) => {
+router.post("/sign-in", (req, res, next) => {
   let userId;
   const { email, password } = req.body;
   User.findOne({ email })
     .then(user => {
       if (!user) {
-        return Promise.reject(new Error("There's no user with that email."));
-      } else {
+        return Promise.reject(new Error("User not found!"));
+      } 
+      else {
         userId = user._id;
         return bcryptjs.compare(password, user.passwordHash);
       }
@@ -51,9 +106,10 @@ router.post('/sign-in', (req, res, next) => {
     .then(result => {
       if (result) {
         req.session.user = userId;
-        res.redirect('/');
-      } else {
-        return Promise.reject(new Error('Wrong password.'));
+        res.redirect("/");
+      } 
+      else {
+        return Promise.reject(new Error("Wrong password."));
       }
     })
     .catch(error => {
@@ -61,15 +117,19 @@ router.post('/sign-in', (req, res, next) => {
     });
 });
 
-router.post('/sign-out', (req, res, next) => {
+router.post("/sign-out", (req, res, next) => {
   req.session.destroy();
-  res.redirect('/');
+  res.redirect("/");
 });
 
-const routeGuard = require('./../middleware/route-guard');
+router.get("/profile", (req, res, next) => {
+  res.render("profile");
+});
 
-router.get('/private', routeGuard, (req, res, next) => {
-  res.render('private');
+const routeGuard = require("../middleware/route-guard");
+
+router.get("/profile", routeGuard, (req, res, next) => {
+  res.render("profile");
 });
 
 module.exports = router;
