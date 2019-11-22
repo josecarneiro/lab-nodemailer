10bfb224e38fa2cfffffcff6564c73b224d94ce2
Eleni Zoumpoulaki <elenizoump@hotmail.com>
2019-11-22 12:33:32 +0000

    Iterations completed, confirmation redirection not working

--- starter-code/routes/authentication.js
+++ starter-code/routes/authentication.js
@@ -1,4 +1,6 @@
-const { Router } = require('express');
+const {
+  Router
+} = require('express');
 const router = new Router();
 
 const User = require('./../models/user');
@@ -12,24 +14,90 @@ router.get('/sign-up', (req, res, next) => {
   res.render('sign-up');
 });
 
+const generateId = length => {
+  const characters =
+    '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let token = '';
+  for (let i = 0; i < length; i++) {
+    token += characters[Math.floor(Math.random() * characters.length)];
+  }
+  return token;
+};
+
+const token = generateId(10);
+
+const nodemailer = require('nodemailer');
+
+const EMAIL = 'ih174test@gmail.com';
+const PASSWORD = 'IH174@lis';
+
+const transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: EMAIL,
+    pass: PASSWORD
+  }
+});
+
+
 router.post('/sign-up', (req, res, next) => {
-  const { name, email, password } = req.body;
+  const {
+    name,
+    email,
+    password
+  } = req.body;
   bcryptjs
     .hash(password, 10)
     .then(hash => {
       return User.create({
         name,
         email,
-        passwordHash: hash
+        passwordHash: hash,
+        confirmationCode: token
       });
     })
     .then(user => {
       req.session.user = user._id;
+      transporter.sendMail({
+        from: `IH Test <${EMAIL}>`,
+        to: email,
+        subject: 'Test email',
+        // text: 'This should be the body of the text email'
+        html: `
+        <style>
+          h1 {
+            /* color: green !important; */
+          }
+        </style>
+        <h1 style="color: green">This should be the body of the text email</h1>
+        <p><strong>Hello</strong> <em>World!</em></p>
+        <a href = "http://localhost:3900/auth/confirm/${token}">confirm here</a>
+      `
+      })
       res.redirect('/');
     })
     .catch(error => {
       next(error);
     });
+});
+
+router.get('/confirm/:confirmCode', (req, res, next) => {
+  const confirmationCode = req.params.confirmCode
+  User.findOne({
+      confirmationCode: confirmationCode
+    })
+    .then(user => {
+      User.update({
+          _id: user._id
+        }, {
+          status: 'Active'
+        })
+        .then(() => {
+          res.render('confirm', user);
+        })
+        .catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
 });
 
 router.get('/sign-in', (req, res, next) => {
@@ -38,8 +106,13 @@ router.get('/sign-in', (req, res, next) => {
 
 router.post('/sign-in', (req, res, next) => {
   let userId;
-  const { email, password } = req.body;
-  User.findOne({ email })
+  const {
+    email,
+    password
+  } = req.body;
+  User.findOne({
+      email
+    })
     .then(user => {
       if (!user) {
         return Promise.reject(new Error("There's no user with that email."));
