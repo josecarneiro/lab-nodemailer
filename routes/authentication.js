2b594e16cc44b11cfcc88bb814f22dd744e7f4d2
Fabio Guerreiro <ffsguerreiro@hotmail.com>
2020-05-12 19:07:14 +0100

    Solved how to send email, problems with verify confirmation code

--- routes/authentication.js
+++ routes/authentication.js
@@ -4,6 +4,13 @@ const router = new Router();
 const User = require('./../models/user');
 const bcryptjs = require('bcryptjs');
 
+//NODEMAILER CONFIGURATION------------------------------
+const dotenv = require('dotenv');
+dotenv.config();
+
+const nodemailer = require('nodemailer');
+//--------------------------------------------------------
+
 router.get('/', (req, res, next) => {
   res.render('index');
 });
@@ -14,22 +21,69 @@ router.get('/sign-up', (req, res, next) => {
 
 router.post('/sign-up', (req, res, next) => {
   const { name, email, password } = req.body;
+
+  const characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let token = '';
+  for (let i = 0; i < 20; i++) {
+    token += characters[Math.floor(Math.random() * characters.length)];
+  }
+
   bcryptjs
     .hash(password, 10)
-    .then(hash => {
+    .then((hash) => {
       return User.create({
         name,
         email,
-        passwordHash: hash
+        passwordHash: hash,
+        confirmationCode: token
       });
     })
-    .then(user => {
+    .then((user) => {
       req.session.user = user._id;
-      res.redirect('/');
+
+      const transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+          user: process.env.NODEMAILER_EMAIL,
+          pass: process.env.NODEMAILER_PASSWORD
+        }
+      });
+      console.log(email);
+      console.log(user.confirmationCode);
+      console.log(`http://localhost:3000/auth/confirm/${user.confirmationCode}`);
+
+      transporter
+        .sendMail({
+          from: `test <${process.env.NODEMAILER_EMAIL}>`,
+          to: email,
+          subject: 'Confirmation email',
+          //text: 'test'
+          html: `<p><a href="http://localhost:3000/auth/confirm/${user.confirmationCode}">Confirm</a></p>`
+        })
+        .then((response) => {
+          console.log(response);
+          req.session.user = user._id;
+          res.redirect('/');
+        })
+        .catch((error) => {
+          next(error);
+        });
     })
-    .catch(error => {
+    .catch((error) => {
       next(error);
     });
+});
+
+router.get('/confirm/:confirmCode', (req, res, next) => {
+  let confirm = req.params.confirmCode;
+  console.log(confirm);
+  //return User.findOneAndUpdate({ confirmationCode: confirm }, { status: 'Active' })
+  //.then((document) => {
+  //console.log(document);
+  //})
+  //.catch((error) => {
+  //next(error);
+  //});
 });
 
 router.get('/sign-in', (req, res, next) => {
@@ -40,7 +94,7 @@ router.post('/sign-in', (req, res, next) => {
   let userId;
   const { email, password } = req.body;
   User.findOne({ email })
-    .then(user => {
+    .then((user) => {
       if (!user) {
         return Promise.reject(new Error("There's no user with that email."));
       } else {
@@ -48,7 +102,7 @@ router.post('/sign-in', (req, res, next) => {
         return bcryptjs.compare(password, user.passwordHash);
       }
     })
-    .then(result => {
+    .then((result) => {
       if (result) {
         req.session.user = userId;
         res.redirect('/');
@@ -56,7 +110,7 @@ router.post('/sign-in', (req, res, next) => {
         return Promise.reject(new Error('Wrong password.'));
       }
     })
-    .catch(error => {
+    .catch((error) => {
       next(error);
     });
 });
